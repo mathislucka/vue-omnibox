2372df7317fbda08cc7e1b690a6acc13560bec0f
mathis <mathis.lucka@gmail.com>
2020-08-26 12:45:01 +0200

    remove search icon; fix vertical alignment

--- src/Omnibox.vue
+++ src/Omnibox.vue
@@ -4,12 +4,7 @@
       class="om-search-container"
       :class="{ 'om-has-focus': isInputFocused }"
       :style="borderColorStyle">
-      <button type="button" class="om-search" @click.prevent="triggerSearch">
-        <slot name="icon">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M15.7 13.3l-3.81-3.83A5.93 5.93 0 0013 6c0-3.31-2.69-6-6-6S1 2.69 1 6s2.69 6 6 6c1.3 0 2.48-.41 3.47-1.11l3.83 3.81c.19.2.45.3.7.3.25 0 .52-.09.7-.3a.996.996 0 000-1.41v.01zM7 10.7c-2.59 0-4.7-2.11-4.7-4.7 0-2.59 2.11-4.7 4.7-4.7 2.59 0 4.7 2.11 4.7 4.7 0 2.59-2.11 4.7-4.7 4.7z"></path></svg>
-        </slot>
-      </button>
-      <div
+      <span
         role="textbox"
         ref="input"
         class="om-input single-line"
@@ -21,7 +16,7 @@
         @keydown.stop="runSpecialKeys"
         @focusin="isInputFocused = true"
         @focusout="isInputFocused = false">
-      </div>
+      </span>
       <span
         v-if="isPlaceholderVisible"
         @click.stop.prevent="focusInput"
@@ -251,10 +246,22 @@ export default {
   .om-root {
     position: relative;
   }
+
+  .om-input {
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
   .om-input:focus {
     border: none;
     outline: none;
   }
+
+  /* This is needed because in firefox the caret position is not vertically centered when the span is empty */
+  .om-input:before {
+    content: ' '
+  }
+
   .om-completion {
     height: 100%;
     width: auto;
@@ -280,7 +287,6 @@ export default {
 
   .om-width--auto {
     width: auto;
-    float: left;
     min-width: 10px;
   }
 
@@ -295,6 +301,8 @@ export default {
     width: 100%;
     float: left;
     color: darkgray;
+    display: flex;
+    align-items: center;
   }
   .om-list-container {
     position: relative;
@@ -321,18 +329,12 @@ export default {
     background-color: lightgray;
   }
 
-  .om-search {
-    vertical-align: middle;
-    background-color: transparent;
-    border: none;
-    overflow: hidden;
-  }
-
   .om-search-container {
     border: 1px solid lightgray;
     display: flex;
     padding: 4px 8px;
     border-radius: 3px;
+    height: 100%;
   }
 
   .om-has-focus {
